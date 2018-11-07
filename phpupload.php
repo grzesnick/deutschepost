5912952307385010fcb497749d910c095aa8b3d3
Grzegorz Adamus <grzegorz0adamus@gmail.com>
2018-11-07 14:00:54 +0100

    1.2 Working on foreach (Lines: 60-70)

--- phpupload.php
+++ phpupload.php
@@ -4,30 +4,70 @@ include 'vendor/autoload.php';
 
 require_once('parsecsv.lib.php');
 
+function strrevpos($instr, $needle)
+{
+    $rev_pos = strpos (strrev($instr), strrev($needle));
+    if ($rev_pos===false) return false;
+    else return strlen($instr) - $rev_pos - strlen($needle);
+};
+
+function after ($this_char, $inthat)
+{
+    if (!is_bool(strpos($inthat, $this_char)))
+        return substr($inthat, strpos($inthat,$this_char)+strlen($this_char));
+};
+
+function after_last ($this_char, $inthat)
+{
+    if (!is_bool(strrevpos($inthat, $this_char)))
+        return substr($inthat, strrevpos($inthat, $this_char)+strlen($this_char));
+};
+
+function before ($this_char, $inthat)
+{
+    return substr($inthat, 0, strpos($inthat, $this_char));
+};
+
 $parser = new \Smalot\PdfParser\Parser();
 $pdf = $parser->parseFile('upload/pdf.pdf');
 
-// Get CSV
-$csv = array_map('str_getcsv', file('upload/filex.csv'));
-$count_row = count($csv);
-
 // Retrieve all pages from the pdf file.
-$pages  = $pdf->getPages();
+$pages = $pdf->getPages();
 $data_ar = array();
-
 // Loop over each page to extract text.
 foreach ($pages as $key => $page) {
     $text = $page->getText();
-    $data_ar[$key] = $text;
+    $data_ar[$key] = after_last('TTENSTADT',$text);
+    echo $data_ar[$key];
+    echo "<br>";
 }
 
-// Search for text before first delimeter in CSV
-$i=2;
-while ($i<$count_row) {
-    $value = substr ('$csv[$i][0]',0, strpos ('$csv[$i][0]', ';'));
+// Get CSV
+$csv = array_map('str_getcsv', file('upload/filex.csv'));
 
+
+$count_row = count($csv);
+$i=2;
+$k=0;
+while ($i<$count_row) {
     echo $csv[$i][0];
+    $csv_ar[$k]=$csv[$i][0];
+    echo "<br>";
     $i++;
+    $k++;
+}
+
+// Search for values in CSV
+foreach ($data_ar as $data_value){
+    $data_value_a = before (" ", $data_value);
+    $data_value_b = after (" ", $data_value);
+    echo $data_value_a;
+}
+
+foreach ($csv_ar as $csv_value)     {
+    $csv_value_a = before (" ", $csv_value);
+    $csv_value_b = after (" ", $csv_value);
+    echo $csv_value_a;
 }
 
 
